561663ee634378060530b042dcf8808050ac0f29
Jacob Schmidt <schhmidt.jacob3@gmail.com>
2019-06-25 09:59:13 -0400

    initial

---
+++ hello.py
@@ -0,0 +1,100 @@
+# print is a function that takes in a value and prints it to terminal, after saving. SAVE TO TERMINAL AND TYPE IN PYTHON
+#print("Hello World.")
+
+#input('What is your name? ')
+#sinput takes in a str and prompts the user in terminal/waits for user input
+#print("What's is your name?")
+#strings can be any typeable character
+#this is a string 'blah blah'
+#NOTE for style guide use pep8
+# to check type of variable use type function
+
+# Variable assignment
+# var assignment is noted by a signle equals sign
+#vars should:
+# be lowercase
+# not have a space
+# use_ instead
+# cannot use built-in keywordss
+#snake_case=lower cas letters and an underscore splitting them
+
+#first_name ='jacob'
+#last_name = 'Schmidt'
+#name = 'Jacob Schmidt'
+
+#print(first_name,last_name)
+#print(type(first_name))
+# Numbers
+
+# int
+#is a whole real number
+
+x = 5
+#NOTE x = 5 =! x+"5"
+#float short for floating point number
+#decimal
+y = 5.0
+
+# complex numbers
+
+#com_x = 5+1j
+
+#Booleans 
+#True or false value
+
+#is_hungry = True
+#is_tired = False
+
+#print(is_hungry)
+
+# Truthy or Falsey
+#X is truthy
+#x = 5
+#Y is falsey
+#y = 0
+
+# There are other forms of truthey and falsey
+#e.g : none
+#none is also a value
+# anything greater than 0 is truthy
+# anthing 0 or none is falsey
+#Lunch Challenge
+#a = input('name >')
+#b = input('age >')
+# Math operators 
+# +,-,*,/,%,**,
+#a = 5
+#b = 7
+#add_two = a+b
+#print(add_two)
+
+#sub_two = a - b
+#print(sub_two)
+
+#mult_two = a * b
+#print(mult_two)
+
+#div_two = a / b
+#print(div_two)
+
+#square_rt = a**b
+#print(square_rt)
+
+#reassign
+
+#print(a)
+
+#string concatanation 
+#x = input('First Name')
+#z = input('Second Name')
+#me = x + " " +z
+#print(me)
+
+
+# assignment opperators
+# they allow the operation on a value that is left of the operation w/o explicitly saying so.
+#me = 'your full name is'
+#x = input('First Name')
+#z = input('Second Name')
+#me += x+ " " +z
+print(1)
